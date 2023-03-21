4014160a7011a9385e028bd53d940baa2f85567c
gabikakol <gabriela.kakol@helsinki.fi>
2023-03-21 20:06:15 +0200

    task 6

--- tasks/week2/unicafe/src/tests/maksukortti_test.py
+++ tasks/week2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_card_exists(self):
         self.assertNotEqual(self.kortti, None)
+    
+    def test_card_initial_balance(self):
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_load_money(self):
+        self.kortti.load_money(2000)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 30.00 euroa")
+
+    def test_spend_money_enough(self):
+        self.kortti.spend_money(500)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_spend_money_not_enough(self):
+        self.kortti.spend_money(5000)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_spend_money_true(self):
+        self.assertEqual(self.kortti.spend_money(500), True)
+
+    def test_spend_money_false(self):
+        self.assertEqual(self.kortti.spend_money(5000), False)
