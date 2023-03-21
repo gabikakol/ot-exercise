105d539d0be67971f40c2c5cb38c7d1a7b101995
gabikakol <gabriela.kakol@helsinki.fi>
2023-03-21 18:48:58 +0200

    unicafe downloaded

--- tasks/week2/card/src/tests/card_test.py
+++ tasks/week2/card/src/tests/card_test.py
@@ -3,19 +3,37 @@ from card import Card
 
 class TestCard(unittest.TestCase):
     def setUp(self):
-        print('set up goes here')
+        self.kortti = Card(1000)
 
     def test_constructior_set_balance_correct(self):
-        # inigialize a card with 10 eur (1000 cents)
-        kortti = Card(1000)
-        ans = str(kortti)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
 
-        self.assertEqual(ans, "Kortilla on rahaa 10.00 euroa")
+    def test_saldo_cheap(self):
+        self.kortti.saldo_cheap()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.50 euroa")
 
-    def test_saldo_cheap_correct(self):
-        kortti = Card(1000)
+    def test_saldo_tasty(self):
+        self.kortti.saldo_tasty()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 6.00 euroa")
+
+    def test_saldo_cheap_negative_balance(self):
+        kortti = Card(200)
         kortti.saldo_cheap()
+        self.assertEqual(str(kortti), "Kortilla on rahaa 2.00 euroa")
 
-        self.assertEqual(str(kortti), "Kortilla on rahaa 7.50 euroa")
+    def test_saldo_tasty_negative_balance(self):
+        kortti = Card(300)
+        kortti.saldo_tasty()
+        self.assertEqual(str(kortti), "Kortilla on rahaa 3.00 euroa")
 
-# tbc @ 'a few points'
+    def test_card_load_money_positive(self):
+        self.kortti.load_money(2500)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 35.00 euroa")
+
+    def test_card_load_money_negative(self):
+        self.kortti.load_money(-500)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_card_saldo_exceeds_maximum(self):
+        self.kortti.load_money(20000)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 150.00 euroa")
