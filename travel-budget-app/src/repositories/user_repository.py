5d8f082adff2d49cec278911d15053426a47d8aa
gabikakol <gabriela.kakol@helsinki.fi>
2023-04-03 19:26:35 +0300

    going somewhere...

---
+++ travel-budget-app/src/repositories/user_repository.py
@@ -0,0 +1,46 @@
+from entities.user import User
+from database_connection import get_database_connection
+
+def get_user_by_row(row):
+    return User(row["username"], row["password"]) if row else None
+
+class UserRepository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    def create_user(self, user):
+        cursor = self.connection.cursor()
+
+        cursor.execute(
+            "insert into users (username, password) values (?, ?);",
+            (user.username, user.password)
+        )
+
+        self.connection.commit()
+
+        return user
+    
+    def find_user(self, username):
+
+        cursor = self.connection.cursor()
+
+        cursor.execute(
+            "select * from users where username = ?;",
+            (username,)
+        )
+
+        row = cursor.fetchone()
+
+        return User(row["username"], row["password"]) if row else None
+    
+    def find_all_users(self):
+
+        cursor = self.connection.cursor()
+
+        rows = cursor.execute(
+                "select * from users;"
+            ).fetchall()
+
+        return [User(row["username"], row["password"]) for row in rows]
+
+user_repository = UserRepository(get_database_connection())
