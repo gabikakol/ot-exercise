8007a007bd541e032e9e5aad16b6dd962043edfe
gabikakol <kakol.gabrysia@gmail.com>
2023-04-03 11:53:39 +0300

    ui menu init

--- travel-budget-app/src/index.py
+++ travel-budget-app/src/index.py
@@ -1,13 +1,37 @@
-#from tkinter import Tk
+from tkinter import Tk, ttk
+
+class UI:
+    def __init__(self, root):
+        self._root = root
+
+    def start(self):
+        heading_label = ttk.Label(master=self._root, text="Login")
+
+        username_label = ttk.Label(master=self._root, text="Username")
+        username_entry = ttk.Entry(master=self._root)
+
+        password_label = ttk.Label(master=self._root, text="Password")
+        password_entry = ttk.Entry(master=self._root)
+
+        button = ttk.Button(master=self._root, text="Save")
+
+        heading_label.grid(row=0, column=0, columnspan=2)
+
+        username_label.grid(row=1, column=0)
+        username_entry.grid(row=1, column=1)
+
+        password_label.grid(row=2, column=0)
+        password_entry.grid(row=2, column=1)
+
+        button.grid(row=3, column=0, columnspan=2)
 
 
-def main():
-#    window = Tk()
-#    window.title("Todo application")
+window = Tk()
+window.title("Travel budget app")
 
-#    window.mainloop()
+ui = UI(window)
+ui.start()
 
-    print('hello world')
+window.mainloop()
 
-if __name__ == "__main__":
-    main()
+#python3 src/index.py
