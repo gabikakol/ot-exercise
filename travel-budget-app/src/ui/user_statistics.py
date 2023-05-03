c143b391d194480677f76afd9b4f9bcf5aefbc5b
gabikakol <gabriela.kakol@helsinki.fi>
2023-05-03 15:23:58 +0300

    user stats completed

--- travel-budget-app/src/ui/user_statistics.py
+++ travel-budget-app/src/ui/user_statistics.py
@@ -52,20 +52,30 @@ class UserStats:
             master=self._window, text=f"Spent per day on average: €{(counters[2]/counters[1]):.2f}")
         average_day_label.grid(padx=5, pady=5)
 
+        stats = self.stats_sorting()
+        """
+        stats is a list with following:
+        - the cheapest category at index 0
+        - the most expensive category at index 1
+        - the cheapest expense at index 2
+        - the most expensive expense at index 3
+        at each index: a tuple with category name at index 0 and cost (as a float number) at index 1
+        """
+
         cheapest_category_label = ttk.Label(
-            master=self._window, text="The cheapest category: ")
+            master=self._window, text=f"The cheapest category: {stats[0][0]} (€{(stats[0][1]):.2f})")
         cheapest_category_label.grid(padx=5, pady=5)
 
         expensive_category_label = ttk.Label(
-            master=self._window, text="The most expensive category:")
+            master=self._window, text=f"The most expensive category: {stats[1][0]} (€{(stats[1][1]):.2f})")
         expensive_category_label.grid(padx=5, pady=5)
 
         cheapest_label = ttk.Label(
-            master=self._window, text="The cheapest item:")
+            master=self._window, text=f"The cheapest item: {stats[2][0]} (€{(stats[2][1]):.2f})")
         cheapest_label.grid(padx=5, pady=5)
 
         expensive_label = ttk.Label(
-            master=self._window, text="The most expensive item:")
+            master=self._window, text=f"The most expensive item: {stats[3][0]} (€{(stats[3][1]):.2f})")
         expensive_label.grid(padx=5, pady=5)
 
         back_button = ttk.Button(master=self._window, text="Back to menu")
@@ -103,3 +113,25 @@ class UserStats:
             if exp.trip_id == id:
                 counter += float(exp.amount)
         return counter
+    
+    def stats_sorting(self):
+        dic = {}
+        expenses = expense_repository.find_all_expenses()
+
+        for exp in expenses:
+            if exp.category in dic:
+                dic[exp.category] += float(exp.amount)
+            else:
+                dic[exp.category] = float(exp.amount)
+        
+        cheapest_cat = min(dic.items(), key=lambda item: item[1])
+        expensive_cat = max(dic.items(), key=lambda item: item[1])
+
+        cheapest_exp = min(expenses, key = lambda exp: exp.amount)
+        expensive_exp = max(expenses, key = lambda exp: exp.amount)
+
+        return [cheapest_cat, expensive_cat, (cheapest_exp.description, float(cheapest_exp.amount)), (expensive_exp.description, float(expensive_exp.amount))]
+    
+
+    
+
