a8906a7ef164e2c900e5b6ad3e007f4528e878c9
gabikakol <gabriela.kakol@helsinki.fi>
2023-05-07 22:05:39 +0300

    some more tests

---
+++ travel-budget-app/src/tests/services/user_service_test.py
@@ -0,0 +1,38 @@
+import unittest
+from entities.user import User
+from services.user_service import UserService
+from errors.errors_handling import InvalidCridentialsError, UserExistsError, PasswordsDontMatchError, EmptyInputError
+
+class FakeUserRepo:
+    def __init__(self, users=None):
+        self.users = users or []
+
+    def create_user(self, user):
+        self.users.append(user)
+
+    def find_user(self, username):
+        for user in self.users:
+            if user.username == username:
+                return user
+        return None
+            
+    def find_all(self):
+        return self.users
+    
+    
+class TestUserService(unittest.TestCase):
+    def setUp(self): 
+        self.user_service = UserService(FakeUserRepo())
+        self.user1 = User("gabby", "123")
+
+    def test_create_user(self):
+        pass
+
+    def test_login(self):
+        pass
+
+    def test_logout(self):
+        pass
+
+    def test_get_username(self):
+        pass
